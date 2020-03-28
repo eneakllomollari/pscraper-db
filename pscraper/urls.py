343094608bf5e5be8afffd5c11c7105c20fa0bcf
Enea Kllomollari <ekllomollari@ucdavis.edu>
2020-03-27 20:28:45 -0700

    Add cache_table and @cache_table to url views

--- pscraper/urls.py
+++ pscraper/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
-from .views import VehicleView, SellerView
+from .views import SellerView, VehicleView
 
 urlpatterns = [
-    path('vehicle/', VehicleView.as_view()),
-    path('seller/', SellerView.as_view())
+    path('vehicle/', cache_page(1600)(VehicleView.as_view())),
+    path('seller/', cache_page(1600)(SellerView.as_view())),
 ]
