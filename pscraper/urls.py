fc0a8f6c0a6c94f23c0af95a01f1d072fb45b360
Enea Kllomollari <ekllomollari@ucdavis.edu>
2020-03-29 02:38:22 -0700

    Remove cache_page
    Temporarily

--- pscraper/urls.py
+++ pscraper/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
 
 from .views import SellerView, VehicleView
 
 urlpatterns = [
-    path('vehicle/', cache_page(1600)(VehicleView.as_view())),
-    path('seller/', cache_page(1600)(SellerView.as_view())),
+    path('vehicle/', VehicleView.as_view()),
+    path('seller/', SellerView.as_view()),
 ]
