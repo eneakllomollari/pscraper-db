959d04aa38818dd0927c6610f173179e64166b8a
Enea Kllomollari <ekllomollari@ucdavis.edu>
2020-05-03 16:51:27 -0700

    Add static files

--- pscraperdb/settings.py
+++ pscraperdb/settings.py
@@ -86,6 +86,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = 'staticfiles/'
 
 django_heroku.settings(locals())
