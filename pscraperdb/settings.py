50d661ea5b7a2524c277c2724a81e456b57d4416
Enea Kllomollari <ekllomollari@ucdavis.edu>
2020-05-09 04:03:30 -0700

    Update authentication

--- pscraperdb/settings.py
+++ pscraperdb/settings.py
@@ -30,7 +30,9 @@ INSTALLED_APPS = [
 ]
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
-    'DEFAULT_FILTER_BACKENDS': 'django_filters.rest_framework.DjangoFilterBackend',
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
--- pscraperdb/urls.py
+++ pscraperdb/urls.py
@@ -6,6 +6,7 @@ from rest_framework.documentation import include_docs_urls
 urlpatterns = [
     path('', lambda r: HttpResponseRedirect('api/v1/docs')),
     path('admin/', admin.site.urls),
+    path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.authtoken')),
     path('api/v1/', include('pscraper.urls')),
     path('api/v1/docs/', include_docs_urls(title='Pscraper API'))
